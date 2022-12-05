2c7890e27bcea60ee6b1f12bd59db358da57adc8
trewwxav <98378287+trewwxav@users.noreply.github.com>
2022-12-05 18:40:04 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+first = ["пиздо", "хуе", "ахуе", "ебнуто", "доху", "пере", "срако", "жопо", "ебле", "бляди", "изъ"] #приставка
+second = ["ёби", "пиздо", "хуе", "чмоне", ] #основная часть слова
+third = ["блядский", "хуев", "стый", "глист", "головый"] #окончание
+print(random.choice(first), random.choice(second), random.choice(third), sep='')
+print(random.choice(first), random.choice(second), random.choice(third), sep='')
+print(random.choice(first), random.choice(second), random.choice(third), sep='')
+print(random.choice(first), random.choice(second), random.choice(third), sep='')
+print(random.choice(first), random.choice(second), random.choice(third), sep='')
+print(random.choice(first), random.choice(second), random.choice(third), sep='')
